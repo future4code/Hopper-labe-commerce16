e440984cec334817d0efe5cdc4d4d6a0675e6eea
bruno-macena <bdsm4@hotmail.com>
2022-05-15 00:36:26 -0300

    HTML + CSS + Cards prontos

--- src/CourseCard/index.js
+++ src/CourseCard/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 
-const CourseCard = () => {
+const CourseCard = (props) => {
+    const { photo} = props;
+    
     return (
         <div className="card">
-        <img className="card-logo"  src="/aerolito-voador.jpg" alt="aerolito-voador" />
-        <h1 className="card-title">Aerolito 1</h1>
+        <img className="card-logo"  src={photo} alt="aerolito-voador" />
+            <h1 className="card-title">Aerolito 1</h1>
+            
         <button>Adicionar ao carrinho</button>
     </div>
     )
--- src/Courses/index.js
+++ src/Courses/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import CourseCard from "../CourseCard";
-
+import AerolitoVoador from "../IMG/aerolito-voador.jpg";
+import PedraLeve from "../IMG/pedra-leve.jpg";
+import Container from "../IMG/container-de-asteroides.jpg";
 const Courses = () => {
     return (
         <main>
         <section className="courses-section">
-                <CourseCard />
-                <CourseCard />
-                <CourseCard />
+                <CourseCard photo={AerolitoVoador} />
+                <CourseCard photo={Container}/>
+                <CourseCard photo={PedraLeve}/>
         </section>
     </main>
     )
